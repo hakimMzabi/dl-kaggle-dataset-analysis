3f81a7e731bb29f255b7f2b6d4e9609107f8daae
TheoHd <theo.huchard@gmail.com>
2020-01-05 23:49:31 +0100

    Modified scripts to be launched at root

--- tools/create_new_environment.py
+++ tools/create_new_environment.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     try:
         environment_name = input("Please enter the name of the environment: ")
         print(f"Creating a new environment \"{environment_name}\"...")
-        os.system(f"conda create --name {environment_name} --file ../requirements.txt")
+        os.system(f"conda create --name {environment_name} --file requirements.txt")
     finally:
         print(f"Successfully created a new environment!")
