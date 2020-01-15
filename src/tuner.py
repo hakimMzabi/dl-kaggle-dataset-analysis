d2b71639042332bf1a4ca6f5bef81cb81f0695c4
Theo Huchard <theo.huchard@gmail.com>
2020-01-15 10:16:28 +0100

    Created 'src/tuner.py' a file that generates scenarios of model generation. Created three scenarios as an example of usage

---
+++ src/tuner.py
@@ -0,0 +1,29 @@
+class Tuner:
+    def __init__(self):
+        self.model_types = ["MLP", "ConvNet", "ResNet", "RNN"]
+        self.dropouts = [None, "Descending", "Constant"]
+        self.optimizers = ["SGD", "Adagrad", "Adadelta", "Adam", "Adamax"]
+        self.activation_functions = ["tanh", "relu", "sigmoid", "softmax"]
+        self.epochs = [100, 150, 200]
+
+    def create_scenarios(self, scenario_file_name):
+        path = "./scenarios/" + scenario_file_name
+        try:
+            scenario_file = open(path, "w")
+            for model_type in self.model_types:
+                for dropout in self.dropouts:
+                    for optimizer in self.optimizers:
+                        for activation_function in self.activation_functions:
+                            for epoch in self.epochs:
+                                scenario_file.write(
+                                    f"{model_type},{dropout},{optimizer},{activation_function},{epoch}\n")
+            scenario_file.close()
+        except ValueError:
+            print(f"Could not create a file {path}.")
+        else:
+            print(f"Successfully created the {path} file.")
+
+
+if __name__ == "__main__":
+    tuner = Tuner()
+    tuner.create_scenarios("scenarios_generation_3.csv")
