fdbd471f7ce1fc17d82e8191ff328376c0bc9073
hakim <hakim.mzabi@etu.u-pec.fr>
2020-01-19 17:59:10 +0100

    Cifar10 Models

--- Model/mlp.py
+++ Model/mlp.py
@@ -11,44 +11,6 @@ from tensorflow.keras.losses import *
 from tensorflow.keras.layers import *
 import matplotlib.pyplot as plt
 
-CIFAR_10_BATCH_PATH = "../dataset/cifar-10-batches-py"
-
-'''def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pkl.load(fo, encoding='bytes')
-    return dict
-
-
-def load_cfar10_batch(cifar10_dataset_folder_path, batch_id):
-    with open(cifar10_dataset_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
-        batch = pkl.load(file, encoding='latin1')
-
-    features = batch['data'].reshape((len(batch['data']), 3, 32, 32)).transpose(0, 2, 3, 1)
-    labels = batch['labels']
-
-    return features, labels
-
-
-def keys_of_cfar10(cifar10_path_folder_path, batch_id):
-    with open(cifar10_path_folder_path + '/data_batch_' + str(batch_id), mode='rb') as file:
-        batch = pkl.load(file, encoding='latin1')
-    return batch['data']
-
-
-def keys_of_cfar10_test(cifar10_path_folder_path):
-    with open(cifar10_path_folder_path + '/test_batch', mode='rb') as file:
-        batch = pkl.load(file, encoding='latin1')
-    return batch['data']
-
-# pp.pprint(unpickle("./dataset/cifar-10-python/cifar-10-batches-py/batches.meta"))
-# print(unpickle("./dataset/cifar-10-python/cifar-10-batches-py/data_batch_1"))
-# print(load_cfar10_batch("./dataset/cifar-10-batches-py", 1)[0])
-
-print(keys_of_cfar10_test(CIFAR_10_BATCH_PATH).shape)
-print(keys_of_cfar10_test(CIFAR_10_BATCH_PATH)[0])
-'''
-
-
 # ------loss: 0.0186 - categorical_accuracy: 0.8758 - val_loss: 0.0804 - val_categorical_accuracy: 0.4906---------- :
 # activation 4Tanh / sigmoid; opt:sgd(lr=0.5)/ loss: mse epochs 200 #
 # ----loss: 0.8592 - categorical_accuracy: 0.6889 - val_loss: 2.2219 - val_categorical_accuracy: 0.4577---: epochs:100
