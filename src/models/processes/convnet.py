4eefd126fcd3b31635a3e911de981260ef3759eb
Theo Huchard <theo.huchard@gmail.com>
2020-01-28 12:27:37 +0100

    architecture: deleted the duplicate files in root to transfer the code into models/processes methods.

--- src/models/processes/convnet.py
+++ src/models/processes/convnet.py
@@ -4,14 +4,14 @@ ConvNet process to generate models for the CIFAR-10 dataset.
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import *
-from tensorflow.keras.activations import *
 from tensorflow.keras.datasets import *
 from tensorflow.keras.metrics import *
 from tensorflow.keras.optimizers import *
 from tensorflow.keras.losses import *
+from tensorflow.keras.activations import *
 from tensorflow.keras.layers import *
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.models import *
+from tensorflow.keras import datasets, layers
 from matplotlib import pyplot as plt
 
 TEST_NAME = "tf2_convnet_mlp_epochs=100"
@@ -60,6 +60,62 @@ def create_model():
                   metrics=[categorical_accuracy])
 
     return model
+
+def create_model_2():
+    from tensorflow.keras import datasets, layers, models
+    import matplotlib.pyplot as plt
+
+
+    (x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+    model.summary()
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    history = model.fit(x_train, y_train, epochs=10,
+                        validation_data=(x_test, y_test))
+
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+
+    print(test_acc)
+
+def create_model_3():
+
+    (x_train, y_train), (x_val, y_val) = datasets.cifar10.load_data()
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same', activation=relu, input_shape=(32, 32, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    # couche complétement connectée
+    model.add(layers.Dense(128, activation='relu'))
+    # couche de sortie
+    model.add(layers.Dense(10, activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=10)
 
 
 if __name__ == "__main__":
--- src/models/processes/rnn.py
+++ src/models/processes/rnn.py
@@ -1,3 +1,28 @@
 '''
 RNN process to generate models for the CIFAR-10 dataset.
 '''
+
+from tensorflow.keras.layers import *
+from tensorflow.keras.metrics import *
+from tensorflow.keras.models import *
+from tensorflow.keras import datasets
+
+
+def create_model():
+    regressor = Sequential()
+    regressor.add(LSTM(50, return_sequences=True, input_dim=(32 * 32 * 3)))
+    # regressor.add(Dropout(0.2))
+    regressor.add(LSTM(units=50, return_sequences=True))
+    regressor.add(LSTM(50, return_sequences=True))
+    regressor.add(LSTM(50, return_sequences=True))
+    # couche de sortie
+    regressor.add(Dense(units=10))
+    regressor.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=[sparse_categorical_accuracy])
+
+    return regressor
+
+
+if __name__ == "__main__":
+    (x_train, y_train), (x_val, y_val) = datasets.cifar10.load_data()
+    regressor = create_model()
+    regressor.fit(x_train, y_train, epochs=10, batch_size=2048)
